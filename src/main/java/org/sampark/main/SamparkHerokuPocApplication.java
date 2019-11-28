f249641ce6da089cc369f99ac069adee175c405a
amdevnntc <amdevnntc@gmail.com>
2019-11-28 16:30:28 +0530

    initial commit

---
+++ src/main/java/org/sampark/main/SamparkHerokuPocApplication.java
@@ -0,0 +1,13 @@
+package org.sampark.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamparkHerokuPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamparkHerokuPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sampark/main/SamparkHerokuPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sampark.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamparkHerokuPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
